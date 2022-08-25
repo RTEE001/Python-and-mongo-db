8be96b3524024f05229512d179f37116fe6b3640
Thifhidzi Rammbuda <70268186+RTEE001@users.noreply.github.com>
2022-08-25 22:35:42 +0200

    refactored code

--- mongo_db.py
+++ mongo_db.py
@@ -1,20 +1,30 @@
-from mongoengine import connect, Document, ListField, StringField
+
+from mongoengine import Document, connect, IntField, StringField
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
-PORT = int(os.getenv("PORT"))
-HOST = os.getenv("HOST")
 
+DATABASE = os.getenv('DATABASE')
+def connect_to_database():
+    connect(DATABASE)
+    print("connected")
+connect_to_database()
 
-connect (db = 'UmuziProspects', host = HOST, port = PORT  )
+class UmuziProspects(Document):
+    id = IntField(auto_incriment = True, primary_key = True )
+    visitor_name = StringField(max_length=200, required=True)
+    visitor_age = IntField(min_value=0, max_value=125)
+    date_of_visit =  StringField(max_length=200, required=True)
+    time_of_visit =  StringField(max_length=200, required=True)
+    assistant = StringField(max_length=200, required=True)
+    comments = StringField(max_length=400, required=True)
 
-# class Visitor(Document):
-#     visitor_name = StringField(max_length=50)
-#     visitor_age = StringField(max_length=50)
-#     date_of_visit = StringField(max_length=50)
-#     date_of_visit = StringField(max_length=50)
-#     time_of_visit = StringField(max_length=50)
-#     name_of_assistant = StringField(max_length=50)
-#     comments = StringField(max_length=50)
+    # def __init__(self, visitor_name, visitor_age, date_of_visit, time_of_visit, assistant, comments):
+    #     self.visitor_name = visitor_name
+    #     self.visitor_age = visitor_age
+    #     self.date_of_visit = date_of_visit
+    #     self.time_of_visit = time_of_visit
+    #     self.assistant = assistant
+    #     self.comments = comments
+
 
