c5aba1ad1fddf53b7ceeced7657290581bf56e14
Thifhidzi Rammbuda <70268186+RTEE001@users.noreply.github.com>
2022-08-27 22:27:57 +0200

    refactored code

--- indexing.py
+++ indexing.py
@@ -1,27 +1,49 @@
 from mongo_db import Visitor
 
-def create_visitor(name, age,date, time ,assistant ,comments ):
-    visitor = Visitor(visitor_name =name,visitor_age =age,
-    date_of_visit =date, time_of_visit = time,
-    assistant =assistant,comments =comments)
+
+def create_visitor(id, name, age, date, time, assistant_, comments):
+    visitor = Visitor(
+        id=id,
+        visitor_name=name,
+        visitor_age=age,
+        date_of_visit=date,
+        time_of_visit=time,
+        assistant=assistant_,
+        comments=comments,
+    )
     visitor.save()
 
 def list_visitors():
     visitors = []
     for visitor in Visitor.objects:
-        visitors.append(visitor)
+        visitors.append({"name": visitor.visitor_name, "id": visitor.id})
+
     return visitors
 
-def delete_visitor(name):
-    Visitor.objects(Visitor_name=name).delete()
+
+def delete_visitor(id):
+    Visitor.objects(id=id).delete()
+
 
 def delete_all():
-    Visitor.remove( { } )
+
+    for visitor in Visitor.objects:
+        id = visitor.id
+        delete_visitor(id)
+
 
 def visitor_details(id):
     visitor = Visitor.objects(id=id)
     return visitor
 
-    
-update_visitor
 
+def update_visitor(id, name, age, date, time, assistant, comments):
+    visitor = visitor_details(id)
+    visitor.id = id
+    visitor.name = name
+    visitor.age = age
+    visitor.date = date
+    visitor.time = time
+    visitor.assistant = assistant
+    visitor.comments = comments
+    visitor.save()
