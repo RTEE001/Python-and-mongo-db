70dc2b12bcc1b20ffa4d67b66942c2c8ba5eb1ae
Thifhidzi Rammbuda <70268186+RTEE001@users.noreply.github.com>
2022-08-28 20:01:45 +0200

    refactored code

--- README.md
+++ README.md
@@ -2,3 +2,30 @@ Python and MongoDB (256)
 For raw project instructions see: http://syllabus.africacode.net/projects/python-specific/mongo/
 
 ## Setup
+
+- Create a ".env" file in the main directory
+- Have the variables DATABASE_NAME, HOST, USERNAME, PASSWORD
+- Assign values to the variables, for this project, the DATABASE_NAME is "UmuziProspects, the host can be any valid host, but for this project simply "localhost:27017" will do. The USERNAME and PASSWORD can be anything of your choice, used in the docker file.
+- Install the requirements from the main directory using
+
+```
+pip3 install -r requirements.txt
+```
+
+- get the docker running using
+
+```
+docker compose up
+```
+
+- Run the source script from the src directory
+
+```
+python3 mongo_db.py
+```
+
+- If you need a GUI representation, you can download mongo compass, where you will use the default connection string
+
+```
+mongodb://<HOST>
+```
--- tests/test_mongo.py
+++ tests/test_mongo.py
@@ -1,48 +1,50 @@
 from unittest.mock import patch
 import unittest
 
+
 class TestIndex(unittest.TestCase):
-    @patch('src.indexing.create_visitor')
+    @patch("src.mongo_db.create_visitor")
     def test_create_visitor(self, mock_visitor):
         mock_visitor("John", 23, "16:00", "2022", "Wayne", "Good")
         mock_visitor.assert_called()
         mock_visitor.assert_called_with("John", 23, "16:00", "2022", "Wayne", "Good")
         mock_visitor.assert_called_once()
 
-    @patch('src.indexing.list_visitors')
+    @patch("src.mongo_db.list_visitors")
     def test_list_visitors(self, mock_list):
         mock_list()
         mock_list.assert_called()
         mock_list.assert_called_with()
         mock_list.assert_called_once()
 
-    @patch('src.indexing.delete_visitor')
+    @patch("src.mongo_db.delete_visitor")
     def test_delete_visitor(self, mock_delete):
         mock_delete(24)
         mock_delete.assert_called()
         mock_delete.assert_called_with(24)
         mock_delete.assert_called_once()
 
-    @patch('src.indexing.delete_all')
+    @patch("src.mongo_db.delete_all")
     def test_delete_all(self, mock_delete_all):
         mock_delete_all(24)
         mock_delete_all.assert_called()
         mock_delete_all.assert_called_with(24)
         mock_delete_all.assert_called_once()
 
-    @patch('src.indexing.visitor_details')
+    @patch("src.mongo_db.visitor_details")
     def test_visitor_details(self, mock_details):
         mock_details(25)
         mock_details.assert_called()
         mock_details.assert_called_with(25)
         mock_details.assert_called_once()
 
-    @patch('src.indexing.update_visitor')
+    @patch("src.mongo_db.update_visitor")
     def test_update_visitor(self, mock_update):
         mock_update(11, "John", 23, "16:00", "2022", "Wayne", "Good")
         mock_update.assert_called()
         mock_update.assert_called_with(11, "John", 23, "16:00", "2022", "Wayne", "Good")
         mock_update.assert_called_once()
+
 
 if __name__ == "__main__":
     unittest.main()
