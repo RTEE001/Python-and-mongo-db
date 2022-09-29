f797e5d819c217c7f923734618a6d038fe6f5fe7
Thifhidzi Rammbuda <70268186+RTEE001@users.noreply.github.com>
2022-09-29 17:36:29 +0200

    made files cohesive

--- tests/test_mongo_db.py
+++ tests/test_mongo_db.py
@@ -1,5 +1,6 @@
+from unittest import mock
 import unittest
-from src.mongo_db import (
+from src.mongo_functions import (
     Visitor,
     create_visitor,
     visitor_details,
@@ -9,83 +10,18 @@ from src.mongo_db import (
     delete_all,
 )
 
-
-class TestVisitor(unittest.TestCase):
-    def test_create_visitor(self):
-        visitor_present = False
-        create_visitor("John", 22, "20-06-2022", "17:00", "James", "Good")
-
-        for visitor_object in Visitor.objects:
-            if visitor_object.visitor_name == "John":
-                visitor_present = True
-        self.assertTrue(visitor_present)
-
-    def test_visitor_details(self):
-
-        for visitor_object in Visitor.objects:
-            self.assertEqual(
-                visitor_object.id, visitor_details(visitor_object.id)["id"]
-            )
-            self.assertEqual(
-                visitor_object.visitor_name,
-                visitor_details(visitor_object.id)["visitor_name"],
-            )
-            self.assertEqual(
-                visitor_object.visitor_age,
-                visitor_details(visitor_object.id)["visitor_age"],
-            )
-            self.assertEqual(
-                visitor_object.date_of_visit,
-                visitor_details(visitor_object.id)["date_of_visit"],
-            )
-            self.assertEqual(
-                visitor_object.time_of_visit,
-                visitor_details(visitor_object.id)["time_of_visit"],
-            )
-            self.assertEqual(
-                visitor_object.comments, visitor_details(visitor_object.id)["comments"]
-            )
-
-    def test_list_visitors(self):
-        visitors = []
-        for visitor_object in Visitor.objects:
-            visitors.append(
-                {"id": visitor_object.id, "name": visitor_object.visitor_name}
-            )
-        self.assertEqual(visitors, list_visitors())
-
-    def test_update_visitor(self):
-        create_visitor("Chuck", 22, "20-06-2022", "20:00", "Grace", "Good")
-        id = []
-        for visitor_object in Visitor.objects:
-            if visitor_object.visitor_name == "Chuck":
-                id.append(visitor_object.id)
-                update_visitor(
-                    id[0], "This was chuck", 22, "20-06-2022", "21:00", "Grace", "Good"
-                )
-                break
-        changed_name = False
-        for visitor_object in Visitor.objects:
-            if visitor_object.visitor_name == "This was chuck":
-                changed_name = True
-        self.assertTrue(changed_name)
-
-    def test_delete_visitor(self):
-        create_visitor("Loki", 22, "20-06-2022", "18:00", "Thor", "Good")
-        deleted = False
-        id = []
-        for visitor_object in Visitor.objects:
-            if visitor_object.visitor_name == "Loki":
-                id.append(visitor_object.id)
-                delete_visitor(id[0])
-                deleted = True
-                break
-        self.assertTrue(deleted)
-
-    def test_delete_all(self):
-        delete_all()
-        self.assertEqual(len(Visitor.objects()), 0)
+@mock.patch("pymongo.collection.Collection.find")
+def test_name(self, mock_create_visitor):
+    mock_create_visitor.return_value = {
+        "visitor_name":"Victor",
+        "visitor_age":27,
+        "date_of_visit":"2019-02-02",
+        "time_of_visit":"17:00",
+        "assistant":"John",
+        "comments":'great person',
+        }
+    self.assertEqual(mock_create_visitor, visitor_details("Victor",27,"2019-02-02",17,"John",'great person'))
 
 
 if __name__ == "__main__":
-    unittest.main()
+    test_name()
