69491b840c6154498bf3bd331dca572d06d80419
Thifhidzi Rammbuda <70268186+RTEE001@users.noreply.github.com>
2022-09-29 22:14:56 +0200

    refactoring code

--- tests/test_mongo_db.py
+++ tests/test_mongo_db.py
@@ -1,4 +1,5 @@
 from unittest import mock
+from mongoengine import connect, disconnect
 import unittest
 from src.mongo_functions import (
     Visitor,
@@ -9,19 +10,44 @@ from src.mongo_functions import (
     delete_visitor,
     delete_all,
 )
+class TestVisitor(unittest.TestCase):
 
-@mock.patch("pymongo.collection.Collection.find")
-def test_name(self, mock_create_visitor):
-    mock_create_visitor.return_value = {
-        "visitor_name":"Victor",
-        "visitor_age":27,
-        "date_of_visit":"2019-02-02",
-        "time_of_visit":"17:00",
-        "assistant":"John",
-        "comments":'great person',
-        }
-    self.assertEqual(mock_create_visitor, visitor_details("Victor",27,"2019-02-02",17,"John",'great person'))
+    @classmethod
+    def setUpClass(cls):
+        connect('mongoenginetest', host='mongomock://localhost')
 
+    @classmethod
+    def tearDownClass(cls):
+       disconnect()
+    
+    def test_create_visitor(self):
+        visitor = Visitor(visitor_name="Victor",
+        visitor_age=27,
+        date_of_visit="2019-02-02",
+        time_of_visit="17:00",
+        assistant="John",
+        comments='great person')
+        visitor.save()
+        fresh_pers = Visitor.objects().first()
+        self.assertEqual(fresh_pers.visitor_name, "Victor")
 
+    def test_update_visitor(self):
+        create_visitor("Chuck", 22, "20-06-2022", "20:00", "Grace", "Good")
+        
+        id = Visitor.objects.filter(visitor_name = "Chuck")[0]
+        update_visitor(id, "James", 22, "20-06-2022", "21:00", "Grace", "Good")
+        self.assetEquals(Visitor.objects.filter(id = id).visitor_name, "James")
+
+    def test_delete_visitor(self):
+        create_visitor("Loki", 22, "20-06-2022", "18:00", "Thor", "Good")
+        self.assertTrue(len(Visitor.objects.filter(visitor_name = "Loki"))==1)
+        delete_visitor(Visitor.objects().first().id)
+        self.assertTrue(len(Visitor.objects.filter(visitor_name = "Loki"))==0)
+     
+      
+    def test_delete_all(self):
+        self.assertNotEqual(len(Visitor.objects()), 0)
+        delete_all()
+        self.assertEqual(len(Visitor.objects()), 0)
 if __name__ == "__main__":
-    test_name()
+    unittest.main()
