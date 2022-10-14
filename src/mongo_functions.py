d165279f894ae2abe418a27d3cb7d13cfc5f4f35
Thifhidzi Rammbuda <70268186+RTEE001@users.noreply.github.com>
2022-10-14 13:19:28 +0200

    used black formatter

--- src/mongo_functions.py
+++ src/mongo_functions.py
@@ -1,5 +1,6 @@
 from src.mongo_connection import Visitor
 
+
 def create_visitor(name, age, date, time, assistant, comments):
     visitor = Visitor(
         visitor_name=name,
@@ -29,16 +30,16 @@ def delete_all():
 
 
 def visitor_details(id):
-    visitor= Visitor.objects(id=id)[0]
+    visitor = Visitor.objects(id=id)[0]
     return {
-            "id": visitor.id,
-            "visitor_name": visitor.visitor_name,
-            "visitor_age": visitor.visitor_age,
-            "date_of_visit": visitor.date_of_visit,
-            "time_of_visit": visitor.time_of_visit,
-            "assistant": visitor.assistant,
-            "comments": visitor.comments,
-        }
+        "id": visitor.id,
+        "visitor_name": visitor.visitor_name,
+        "visitor_age": visitor.visitor_age,
+        "date_of_visit": visitor.date_of_visit,
+        "time_of_visit": visitor.time_of_visit,
+        "assistant": visitor.assistant,
+        "comments": visitor.comments,
+    }
 
 
 def update_visitor(id, name, age, date, time, assistant, comments):
